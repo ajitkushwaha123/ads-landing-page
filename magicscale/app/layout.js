f9c884658e415ccfdf69e7a61c6a8f09e4b271c3
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2024-12-04 00:00:55 +0530

    facebook Pexels

--- magicscale/app/layout.js
+++ magicscale/app/layout.js
@@ -14,6 +14,26 @@ export default function RootLayout({ children }) {
           name="facebook-domain-verification"
           content="dpmcab7a29fw9hk1vhwxi6xn0gi9d0"
         />
+        <script>
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '553409800818621');
+          fbq('track', 'PageView');`}
+        </script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=553409800818621&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className="antialiased">{children}</body>
     </html>
