4b8a8417190a21ecfe02173558125cca7af5aa7d
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2024-12-03 23:03:57 +0530

    facebook verification code

--- magicscale/app/page.js
+++ magicscale/app/page.js
@@ -4,10 +4,17 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Return from "@/components/Return";
 import Reviews from "@/components/Reviews";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <meta
+          name="facebook-domain-verification"
+          content="4jwtpgw6eugrzzjm57eddyxmy6z1io"
+        />
+      </Head>
       <Hero />
       <Features />
       <Reviews />
